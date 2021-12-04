7c50cba615d97115d3c33b9ca9748fd268b5e0dd
= <=>
2021-12-04 18:33:12 +0100

    preparing V1.0.1

--- doc/postgresql/populate.sql
+++ doc/postgresql/populate.sql
@@ -1,8 +1,6 @@
--- apply after schema.sql and before migration scripts.
--- update versions with their associated migration script.
-INSERT INTO horcrux_versions (number, script) VALUES 
-    (1 , NULL) 
---    (2, 'upgrate_to_v2.sql'),
---    (3, 'upgrate_to_v3.sql')
-ON CONFLICT (number) DO UPDATE SET script = EXCLUDED.script 
+ -- applied when database already exists, just before migration scripts.
+ -- known versions with their associated migration script.
+INSERT INTO horcrux_versions (number, script) VALUES
+    (2, 'upgrate_to_v2.sql')
+ON CONFLICT (number) DO UPDATE SET script = EXCLUDED.script
 WHERE horcrux_versions.number = EXCLUDED.number;
--- doc/postgresql/schema.sql
+++ doc/postgresql/schema.sql
@@ -1,20 +1,21 @@
--- apply before all other scripts.
+-- applied only if database is empty.
+-- creates schema, insert predefined records
 CREATE TABLE IF NOT EXISTS horcrux_versions (
-	number INT NOT NULL UNIQUE,
-	script VARCHAR(64),
-	active BOOLEAN
+    number INT NOT NULL UNIQUE,
+    script VARCHAR(64),
+    active BOOLEAN
 );
 
--- set to true the current version of the database (replace 1 with the current version of database)
-INSERT INTO horcrux_versions (number, script, active) VALUES (1 , NULL, true)
+CREATE TABLE IF NOT EXISTS horcrux_users (
+    id UUID PRIMARY KEY,
+    name VARCHAR(64),
+    profile VARCHAR(32)
+    email VARCHAR(32)
+);
+
+-- updates the current version of the database
+INSERT INTO horcrux_versions (number, script, active) VALUES (2 , NULL, true)
 ON CONFLICT (number) DO UPDATE
     SET script = EXCLUDED.script, active=true
     WHERE horcrux_versions.number=EXCLUDED.number;
 
--- do other tables and constraints creation
---CREATE TABLE IF NOT EXISTS horcrux_users (
---    id UUID PRIMARY KEY,
---    name VARCHAR(64),
---    profile VARCHAR(32)
---    email VARCHAR(32)
---);
